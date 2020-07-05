997cb60b8f2f8a12c32d4c8521bbb80b274dae60
AuthurZhang <289601639@qq.com>
2020-07-05 21:51:17 +0800

    上传项目

---
+++ Test/app/src/main/java/com/example/test/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn = findViewById(R.id.button1);
+        final TextView tv = findViewById(R.id.tv_title);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tv.setText("I love bytedance");
+            }
+        });
+    }
+}
